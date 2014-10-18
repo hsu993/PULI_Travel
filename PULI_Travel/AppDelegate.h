997351668941a15db978342efe3e013a6d779ad3
viplab <viplab@ip153.puli25-103-10.ncnu.edu.tw>
2014-10-18 20:24:59 +0800

    Initial Commit

---
+++ PULI_Travel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PULI_Travel
+//
+//  Created by viplab on 2014/10/18.
+//  Copyright (c) 2014年 viplab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PULI_Travel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PULI_Travel
+//
+//  Created by viplab on 2014/10/18.
+//  Copyright (c) 2014年 viplab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
